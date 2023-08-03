56a68b6a05f830bc979f49db886f83104c94c665
Laura Dinh <56850508+lauradinh@users.noreply.github.com>
2023-08-02 20:36:04 -0500

    Upload files

---
+++ src/main/java/dev/lauramdinh/movies/Movie.java
@@ -0,0 +1,30 @@
+package dev.lauramdinh.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // takes care of getters and setters
+@AllArgsConstructor // creates constructors with all private fields
+@NoArgsConstructor
+public class Movie {
+    @Id // unique identifier
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference // store ids of reviews and reviews will be in separate
+    private List<Review> reviewIds;
+
+}
